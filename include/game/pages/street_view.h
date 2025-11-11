4b8276a4bfe634bf8dd2525ccf48e284f372fe32
kforczek <kuba.forczek@gmail.com>
2025-11-11 23:34:33 +0100

    Map resizing - not perfect yet

--- include/game/pages/street_view.h
+++ include/game/pages/street_view.h
@@ -21,26 +21,44 @@ public:
     explicit StreetViewPage(QWidget* parent, const geo::Location& mapCenter);
 
     const geo::Location& getStreetViewLocation() const;
-
     void startNewRound(const geo::Location& location);
 
 signals:
     void guessMade(const geo::Location& location);
 
 protected:
+    void showEvent(QShowEvent *event) override;
     void resizeEvent(QResizeEvent* event) override;
 
-private:
+private /*fields*/:
     sv::StreetView m_streetView;
     sv::InteractiveMap m_interactiveMap;
 
     QPushButton m_returnToStartButton;
     QPushButton m_guessButton;
 
+    QPushButton m_shrinkMapButton;
+    QPushButton m_enlargeMapButton;
+
+    double m_mapWidthPercent = 0;
+    double m_mapHeightPercent = 0;
+
+private /*methods*/:
+    void resizeStreetView();
+    void resizeAndMoveMap(int newWidth = 0, int newHeight = 0);
+    void moveMapSizeButtons();
+    void moveFunctionalButtons();
+
+    void updateMapRatios();
+
 private slots:
     void onGuessMarkerPlaced();
+
     void onGuessButtonClicked();
     void onReturnToStartButtonClicked();
+
+    void onShrinkMapButtonClicked();
+    void onEnlargeMapButtonClicked();
 };
 
 }
